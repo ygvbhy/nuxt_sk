079f149219a3616c27c299879bf7b4ece17ff979
ygvbhy <ygvbhy@naver.com>
2024-05-16 21:51:01 +0900

    nuxt-link 태그 소개

--- components/HeaderLayout.vue
+++ components/HeaderLayout.vue
@@ -3,7 +3,7 @@
     <h1>{{ $route.name }} 페이지</h1>
     <nuxt-link to="/">홈페이지</nuxt-link>
     <nuxt-link to="/main">메인 페이지</nuxt-link>
-    <nuxt-link to="/product">상품페이지</nuxt-link>
+    <!-- <nuxt-link to="/product">상품페이지</nuxt-link> -->
   </div>
 </template>
 
