0a19d55df8789469fb24b4fe025a7e25bf43517e
ygvbhy <ygvbhy@naver.com>
2024-08-21 21:33:37 +0900

    asyncData 속성 주의 사항 ( pages 폴더 안의 vue 파일에만 작성 가능 )

--- pages/main.vue
+++ pages/main.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
     <p>메인 페이지 입니다.</p>
-    <div>
-      {{ products }}
-    </div>
+    <ProductList />
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import ProductList from '../components/ProductList.vue'
 export default {
+  components: { ProductList },
   async asyncData() {
     const response = await axios.get('http://localhost:3000/products')
-    console.log(response)
     const products = response.data
     return { products }
   },
