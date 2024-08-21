30fd78450cc7d93eb4b54304f86c01770ec6626a
ygvbhy <ygvbhy@naver.com>
2024-08-21 21:47:20 +0900

    상품 목록 표시 기능 구현

--- pages/main.vue
+++ pages/main.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
     <p>메인 페이지 입니다.</p>
-    <ProductList />
+    <div>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <img :src="product.imageUrl" :alt="product.name" />
+          <p>{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import ProductList from '../components/ProductList.vue'
+// import ProductList from '../components/ProductList.vue'
 export default {
-  components: { ProductList },
+  // components: { ProductList },
   async asyncData() {
     const response = await axios.get('http://localhost:3000/products')
-    const products = response.data
+    const products = response.data.map((item) => ({
+      ...item,
+      imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+    }))
     return { products }
   },
 }
