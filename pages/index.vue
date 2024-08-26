eb3d8e9621af3066cbbfb2f5040d2061381c02a3
ygvbhy <ygvbhy@naver.com>
2024-08-26 20:49:12 +0900

    상품 목록 표시 기능 구현 ssr 오류로 nuxt.config 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-    <p>넉스트 시작하기</p>
+    <p>메인 페이지 입니다.</p>
+    <div>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <img :src="product.imageUrl" :alt="product.name" />
+          <p>{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+// import ProductList from '../components/ProductList.vue'
+export default {
+  // components: { ProductList },
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response.data.map((item) => ({
+      ...item,
+      imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+    }))
+    return { products }
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
