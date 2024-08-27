04dc93d9b51d24e7fec4503aafeed48c07aa0021
ygvbhy <ygvbhy@naver.com>
2024-08-27 21:27:57 +0900

    상품 상세 정보 조회 기능 개발

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
     <h1>상세 페이지</h1>
+    <div>
+      <img :src="product.imageUrl" :alt="product.name" />
+      <p>name: {{ product.name }}</p>
+      <p>price: {{ product.price }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { fetchProductById } from '@/api'
 export default {
-  created() {
-    console.log(this.$route)
+  async asyncData({ params }) {
+    const response = await fetchProductById(params.id)
+    const product = response.data
+    return { product }
   },
 }
 </script>
