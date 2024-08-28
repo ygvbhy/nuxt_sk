30696ce1547fe1f1b26d36a688d795de1e48f4db
ygvbhy <ygvbhy@naver.com>
2024-08-28 21:50:08 +0900

    상품 상세 페이지 레이아웃 정리 및 전역 스타일시트 설정

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <h1>상세 페이지</h1>
-    <div>
-      <img :src="product.imageUrl" :alt="product.name" />
-      <p>name: {{ product.name }}</p>
-      <p>price: {{ product.price }}</p>
+    <div class="container">
+      <div class="main-panel">
+        <img
+          class="product-image"
+          :src="product.imageUrl"
+          :alt="product.name"
+        />
+      </div>
+      <div class="side-panel">
+        <p class="name">{{ product.name }}</p>
+        <p class="price">{{ product.price }}</p>
+        <!-- <button type="button" @click="addToCart">Add to Cart</button> -->
+      </div>
     </div>
   </div>
 </template>
@@ -20,4 +28,22 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+.product-image {
+  width: 500px;
+  height: 375px;
+}
+.side-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 220px;
+  text-align: center;
+  padding: 0 1rem;
+}
+</style>
